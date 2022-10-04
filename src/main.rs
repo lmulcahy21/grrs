1c3bb289da8522ae50c40af0ddcfb374d874376b
lmulcahy21 <71798334+lmulcahy21@users.noreply.github.com>
2022-10-04 01:40:08 -0400

    working buffer version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use clap::Parser;
+use std::io::prelude::*;
 
 #[derive(Parser)]
 #[derive(Debug)]
@@ -21,12 +22,20 @@ fn main() {
     let args: Cli = Cli::parse();
     println!("{} {}",args.pattern,args.path.display());
 
-    let content: String = std::fs::read_to_string(&args.path).expect("Failed to read file");    
+    //let content: String = std::fs::read_to_string(&args.path).expect("Failed to read file");
+    //attempting some optimization through ReadBuf instead of reading whole file into memory
+    let f = std::fs::File::open(&args.path).expect("Failed to open file");
+    let mut reader = std::io::BufReader::new(f);
 
-    for line in content.lines() {
+    let mut line: String = String::new();
+    let mut len = reader.read_line(&mut line);
+
+    while len.unwrap() > 0 {
         if line.contains(&args.pattern) {
             println!("{}",line);
         }
+        line.clear();
+        len = reader.read_line(&mut line);
     }
 
 }
